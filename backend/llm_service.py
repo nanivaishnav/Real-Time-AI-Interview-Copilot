7c3c447ba442faaccbe1ed857cb910c6ad551264
Nani Vaishnav <nanivaishnav45@gmail.com>
2026-04-05 18:05:46 +0530

    Added LLM service logic

--- backend/llm_service.py
+++ backend/llm_service.py
@@ -1,1 +1,27 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
 
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv("**** **** *** *** **** ****"))
+
+def analyze_answer(user_answer: str):
+    prompt = f"""s
+    You are an interview coach.
+
+    Analyze this answer:
+    "{user_answer}"
+
+    Give:
+    1. Feedback (clarity, depth, missing points)
+    2. Improved version of the answer
+    """
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{"role": "user", "content": prompt}],
+        temperature=0.7
+    )
+
+    return response.choices[0].message.content
