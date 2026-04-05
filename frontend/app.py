7c6a29e383d7082386d1bae82f971148734bbbbe
Nani Vaishnav <nanivaishnav45@gmail.com>
2026-04-05 18:15:52 +0530

    Added streamlit

--- frontend/app.py
+++ frontend/app.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import requests
 
+st.title("🎤 AI Interview Copilot (MVP)")
+
+user_input = st.text_area("Enter your answer:")
+
+if st.button("Analyze"):
+    response = requests.post(
+        "http://127.0.0.1:8000/analyze",
+        json={"answer": user_input}
+    )
+
+    if response.status_code == 200:
+        st.write("### Feedback:")
+        st.write(response.json()["result"])
+    else:
+        st.error("Error connecting to backend")
